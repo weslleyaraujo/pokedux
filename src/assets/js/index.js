efbe3f8175019b1d2948e586bbf10185e44845b4
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-12-09 18:26:44 -0200

    fix(redux-devtools): Adding redux-devtools
    Adding redux devtools bar.
    Didnt add react-hot-loader from now cause it supports only webpack..
    
    Closes #1;

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 import App from './containers/App';
 import store from './store';
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <div>
+    <Provider store={store}>
+      <App />
+    </Provider>
+
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>,
+
   document.querySelector('#app')
 );
--- src/assets/js/store/index.js
+++ src/assets/js/store/index.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { devTools, persistState } from 'redux-devtools';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers/rootReducer';
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunk
+const createStoreWithMiddleware = compose(
+  applyMiddleware(thunk),
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
 )(createStore);
 
 const store = createStoreWithMiddleware(rootReducer);
