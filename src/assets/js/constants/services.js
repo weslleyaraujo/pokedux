6bd3a0bd6a8f204fe43212a9bb61fe0088a5461c
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-12-16 01:49:24 -0200

    fix(constants): Renaming some service constant..

--- src/assets/js/constants/services.js
+++ src/assets/js/constants/services.js
@@ -1,4 +1,4 @@
 export const POKEAPI_ROOT_URL = 'http://pokeapi.co/api';
 export const POKEAPI_POKEDEX_URL = '/v1/pokedex/';
-export const POKEAP_IMAGE_URL = '/media/img/';
+export const POKEAPI_IMAGE_URL = '/media/img/';
 
