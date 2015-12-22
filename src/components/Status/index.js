7c806301fd64a1eed6bc0435bc8c8c467781aad4
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-12-22 19:39:04 -0200

    feat(Status): Starting a status loader

--- src/components/Status/index.js
+++ src/components/Status/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NULL_STATUS } from '../../constants/status';
 import styles from './index.css';
 
-const Status = ({
-  status,
-  message
-}) => {
-  const className = status === NULL_STATUS ? styles.common : styles.active;
+import pikachu from './images/01.gif';
+import other from './images/02.gif';
+import another from './images/03.gif';
+
+const images = [
+  pikachu,
+  other,
+  another
+];
+
+function singlePreload(src, key, onLoad) {
   return (
-    <div className={className} >
-      {message && <p>{message}</p>}
-    </div>
-  )
+    <img
+      key={key}
+      onLoad={onLoad}
+      style={{
+        display: 'none'
+      }}
+     />
+  );
+}
+
+class Status extends Component {
+
+  preLoadImages() {
+    return images.map((x, i) => singlePreload(x, i, (e) => console.log(e)));
+  }
+
+  componendDidMount() {
+  
+  }
+
+  render() {
+    const className = status === NULL_STATUS ? styles.common : styles.active;
+    const { message } = this.props;
+    return (
+      <div className={className} >
+        {message && <p>{message}</p>}
+        {this.preLoadImages()}
+      </div>
+    );
+  }
 }
 
 export default Status;
