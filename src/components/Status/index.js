c29fbc7f7ff905c6aa1b174ad493ad71124d588f
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-12-22 23:06:33 -0200

    feat(Status): Using props instead of state for status
    Chosen to use props instead of state in order to keep the flag of which
    random image will be rendered into the loader.

--- src/components/Status/index.js
+++ src/components/Status/index.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-fetch';
 import { polyfill } from 'es6-promise';
+import { random, times } from 'lodash';
 
 import styles from './index.css';
+import { NULL_STATUS } from '../../constants/status'
 
-import pikachu from './images/01.gif';
-import other from './images/02.gif';
-import another from './images/03.gif';
-
-const images = [
-  pikachu,
-  other,
-  another,
-];
+// TODO: update to something more smart than this.
+const images = times(4).map((x) => require(`./images/0${x}.gif`));
 
 function singlePreload(src) {
   return new Promise((resolve, reject) => {
@@ -23,55 +18,55 @@ function singlePreload(src) {
 }
 
 class Status extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      images,
-      loaded: false
-    }
+  state = {
+    images,
+    actual: 0,
+    loaded: false
   }
 
   componentDidMount() {
     let promises = this.state.images.map((x) => singlePreload(x));
 
     Promise.all(promises)
-      .then(::this.onLoadImages);
+      .then(::this.onImagesLoaded);
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      actual: random(0, images.length - 1)
+    });
   }
 
   onFailImage() {
     console.log('whoops, some image failed');
   }
 
-  onLoadImages(events) {
+  onImagesLoaded(events) {
     this.setState({
       loaded: true
     });
   }
 
   renderImage() {
-    let { images  } = this.state;
-    let src = images[0];
+    let { images, actual  } = this.state;
+    let src = images[actual];
 
     return (
-      <img
-        src={src}
-      />
+      <img src={src} />
     );
   }
 
   render() {
-    const className = !status ? styles.common : styles.active;
-    const { message } = this.props;
+    const { message, status } = this.props;
     const { loaded } = this.state;
-
-    console.log(className, status);
+    const className = status !== NULL_STATUS ? styles.active : styles.common;
 
     return (
       <div className={className} >
         {message && <p>{message}</p>}
         <div>
-          { loaded ? this.renderImage() : '' }
+          {loaded ? this.renderImage() : ''}
         </div>
       </div>
     );
