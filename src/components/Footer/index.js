a0bbf1d9c1128d36b75f3f00004fe6019628d24e
Weslley Araujo <weslleyaraujo20@gmail.com>
2016-01-01 10:34:11 -0200

    feat(footer): Adding footer component.
    Creating a simple footer component.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Footer.css';
+
+const Footer = () => (
+  <footer className={styles.root}>
+    <small>Open source project hosted at <a href="http://github.com/weslleyaraujo/pokedux">Github</a> - <a href="http://twitter.com/_weslleyaraujo">@_weslleyaraujo</a></small>
+  </footer>
+);
+
+export default Footer;
