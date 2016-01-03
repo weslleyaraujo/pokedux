0d6e1e4b14064c85235f6d87cabe2a66088381e8
Weslley Araujo <weslleyaraujo20@gmail.com>
2016-01-03 07:45:55 -0200

    style(add-namespace): Typo.

--- src/helpers/add-namespace.js
+++ src/helpers/add-namespace.js
@@ -10,7 +10,6 @@
  * { foo--example: 'bar' }
  */
 export default function addNamespace(properties = {}, namespace) {
-
   return Object.keys(properties).reduce((c, n) => {
     c[[`${namespace}--${n}`]] = properties[n];
     return c;
