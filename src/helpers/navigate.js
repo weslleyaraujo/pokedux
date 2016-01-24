1d63f1f868b167bde42a6948b80b8b8bd6c5ccc1
Weslley Araujo <weslleyaraujo20@gmail.com>
2016-01-24 23:57:35 +0100

    style(navigate): Removing comment/unused lines.

--- src/helpers/navigate.js
+++ src/helpers/navigate.js
@@ -1,8 +1,5 @@
 export default function navigate(history = {}, path = '/') {
   let { push } = history;
-  // if (!push) {
-  //   throw new Error ('navigate: cant get `push` from history argument');
-  // }
 
   return (event) => {
     event.preventDefault();
