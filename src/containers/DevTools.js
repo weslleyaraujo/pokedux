06d31e69a88ae50ad180a4991dc10661ddc6a102
Weslley Araujo <weslleyaraujo20@gmail.com>
2016-01-25 19:13:31 +0100

    feat(DevTools): Change default position/visibility

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -7,13 +7,12 @@ import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
-// createDevTools takes a monitor and produces a DevTools component
 const DevTools = createDevTools(
-  // Monitors are individually adjustable with props.
-  // Consult their repositories to learn about those props.
-  // Here, we put LogMonitor inside a DockMonitor.
-  <DockMonitor toggleVisibilityKey='ctrl-h'
-               changePositionKey='ctrl-q'>
+  <DockMonitor
+        defaultIsVisible={false}
+        toggleVisibilityKey='ctrl-h'
+        changePositionKey='ctrl-q'
+        defaultPosition='left'>
     <LogMonitor theme='tomorrow' />
   </DockMonitor>
 );
