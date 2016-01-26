30ae1ed3e7d119f89f763e7ec6ba9117d974770b
Weslley Araujo <weslleyaraujo20@gmail.com>
2016-01-26 10:25:57 +0100

    style(webpack): Ident ftw

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -48,13 +48,13 @@ export default {
         ]
       },
 
-			{
-				test: /.*\.(gif|png|jpe?g|svg)$/i,
-				loaders: [
-					'file?hash=sha512&digest=hex&name=[hash].[ext]',
-					'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
-				]
-			}
+      {
+        test: /.*\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+        ]
+      }
     ]
   },
 
