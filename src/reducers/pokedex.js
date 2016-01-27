f4c051416f745321010f3be77ebfb4c686984e0e
Weslley Araujo <weslleyaraujo20@gmail.com>
2016-01-27 10:14:21 +0100

    feat(reducers): Creating reducer handler to change page action

--- src/reducers/pokedex.js
+++ src/reducers/pokedex.js
@@ -1,9 +1,13 @@
+import { chunk } from 'lodash';
+
 import * as actionTypes from '../constants/actionTypes';
 import { pokemon } from './pokemon';
 
 export const INITIAL_STATE = {
   pokemons: []
 };
+
+export const PER_PAGE = 10;
 
 export function pokedex(state = INITIAL_STATE, action) {
   if (!action) {
@@ -15,6 +19,15 @@ export function pokedex(state = INITIAL_STATE, action) {
       let { objects } = action.data;
       let pokemons = objects.reduce((c, n) => [...c, ...n.pokemon], [])
         .map((x) => pokemon(x, { type: actionTypes.POKEMON_UPDATE }));
+
+      return {
+        ...state,
+        pokemons,
+      }
+
+    case actionTypes.CHANGE_PAGE:
+      let { pokemons, currentPage } = action.data;
+      let items = chunk(pokemon, PER_PAGE)[currentPage - 1]
 
       return {
         ...state,
